706ef9fceaa1b96922ccb62078d02f89addfaaa4
Mahija1294 <mahimahija24@gmail.com>
2025-06-06 16:53:34 +0530

    Add files via upload

---
+++ SubjectsProgramCode.java
@@ -0,0 +1,17 @@
+package myfirstpackage;
+
+public class SubjectsProgramCode {
+
+	public static void main(String[] args) {
+     int maths = 97;
+     int science= 89;
+     int english = 93;
+     
+     int total = maths+science+english;
+     int average = total/3;
+     
+     System.out.println("Total Marks : " + total);
+     System.out.println("Average : " +average);
+	}
+
+}
