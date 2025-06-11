0128109ad86782c4997536a412b1e8f55a7738dc
Mahija1294 <mahimahija24@gmail.com>
2025-06-11 15:56:46 +0530

    Add files via upload

---
+++ SumOfDigits.java
@@ -0,0 +1,17 @@
+package myfirstpackage;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		int num = 1234;
+        int sum = 0;
+
+        while (num != 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+	}
+
+}
