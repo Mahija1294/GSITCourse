d08e74cd02781acef0048b1c40168a2a8f56c5cb
Mahija1294 <mahimahija24@gmail.com>
2025-06-11 12:58:10 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,16 @@
+package myfirstpackage;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		int day = 2;
+		switch(day)
+		{
+		case 1: System.out.println("Sunday"); 
+		case 2: System.out.println("Monday"); 
+		case 3: System.out.println("Tuesday"); 
+		default: System.out.println("Wrong Inputs!!"); 
+		}
+	}
+
+}
